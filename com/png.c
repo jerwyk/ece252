f2a9b057f7d9b156611635bd30366b5f0390a989
jerwyk <jerwyk0526@gmail.com>
2020-09-27 03:01:00 -0400

    fixed memset

--- com/png.c
+++ com/png.c
@@ -27,7 +27,7 @@ int get_png_width(struct data_IHDR *buf)
 int read_simple_png(simple_PNG_p *png, FILE *fptr)
 {
     *png = malloc(sizeof(struct simple_PNG));
-    memset(png, 0, sizeof(struct simple_PNG));
+    memset(*png, 0, sizeof(struct simple_PNG));
     /*signature of file*/
     uint8_t sig[PNG_SIG_SIZE];
     fread(&sig, sizeof(sig), 1, fptr);
@@ -52,7 +52,7 @@ int read_simple_png(simple_PNG_p *png, FILE *fptr)
 
         /*IDAT chunk*/
         /*alloc space for idat*/
-        (*png)->p_IDAT = malloc(sizeof(struct chunk));;
+        (*png)->p_IDAT = malloc(sizeof(struct chunk));
         /*read length of chunk*/
         fread(&len_buf, CHUNK_LEN_SIZE, 1, fptr);
         (*png)->p_IDAT->length = ntohl(len_buf);
