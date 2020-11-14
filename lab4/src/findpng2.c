06657a1890fd92fa3b531f1899223427e1ff9a77
niquancong <niquancong@gmail.com>
2020-11-14 14:37:40 -0500

    added thread initialization and timing

--- lab4/src/findpng2.c
+++ lab4/src/findpng2.c
@@ -4,6 +4,7 @@
 #include <curl/curl.h>
 #include <pthread.h>
 #include <search.h>
+#include <sys/time.h>
 #include "png.h"
 
 #define ECE252_HEADER "X-Ece252-Fragment: "
@@ -15,10 +16,16 @@ hashmap_t visited_pngs;
 
 int main(int argc, char **argv)
 {
+	/* used for program timing */
+    struct timeval t1, t2;
+    gettimeofday(&t1, NULL);
+	
     int image_num = 50;
     int thread_num = 1;
     char *logfile = NULL;
     char *seed_url = NULL;
+	void** retval;
+	
     /* parse options */
     if(argc > 1)
     {
@@ -52,13 +59,32 @@ int main(int argc, char **argv)
 
     /* initializations */
     curl_global_init(CURL_GLOBAL_DEFAULT);
+	
     /* create threads to crawl the web */
+	pthread_t* threads = malloc(sizeof(pthread_t) * thread_num);
+	int* thread_status = malloc(sizeof(int) * thread_num);
+	
     for(int i = 0; i < thread_num; ++i)
     {
-
+		// run t_crawler with each thread with argument max
+		thread_status[i] = pthread_create(&threads[i], NULL, t_crawler, max);
     }
+	
+	for(int i = 1; i < thread_num; ++i){
+		if(thread_status[i] == 0){
+			pthread_join(threads[i], retval);
+		}
+	}
 
     /* clean up */
     curl_global_cleanup();
 
+    gettimeofday(&t2, NULL);
+    double t1_sec, t2_sec;
+    t1_sec = t1.tv_sec + t1.tv_usec / 1000000.0;
+    t2_sec = t2.tv_sec + t2.tv_usec / 1000000.0;
+
+    printf("findpng2 execution time: %f seconds\n", t2_sec - t1_sec);
+	
+	return 0;
 }
